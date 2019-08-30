fdb6140ba7afc6847f948e204c699ae08848e921
jakazzy <jakazzy@gmail.com>
2019-08-30 01:25:39 +0000

    add css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import BookList from "./components/BookList";
+import BookForm from "./components/BookForm";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -13,6 +14,7 @@ class App extends Component {
       <div className="App">
         <Navbar books={this.state.books} />
         <BookList books={this.state.books} />
+        <BookForm />
       </div>
     );
   }
--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -3,10 +3,44 @@ import React, { Component } from "react";
 export class BookForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      title: "",
+      author: ""
+    };
   }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log(this.state);
+  };
   render() {
-    return <div></div>;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <input
+          type="text"
+          placeholder="book title"
+          id="title"
+          value={this.state.title}
+          onChange={this.handleChange}
+          required
+        />
+        <input
+          type="text"
+          placeholder="author name"
+          value={this.state.author}
+          id="author"
+          onChange={this.handleChange}
+          required
+        />
+        <input type="submit" value="add book" />
+      </form>
+    );
   }
 }
 
